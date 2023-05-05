f699b83b7b2e9493a801a9248dc92205bae9dd64
Theophilus Adeleke <theophilusadesola002@gmail.com>
2023-05-05 15:03:21 +0100

    CREATE new article and handle the error in creating the article.

--- app.js
+++ app.js
@@ -12,8 +12,14 @@ app.use(express.static("public"))
 mongoose.connect("mongodb://localhost:27017/wikiDB")
 
 const articleSchema = new mongoose.Schema({
-	title: String,
-	content: String
+	title: {
+		type: String,
+		required: true
+	},
+	content: {
+		type: String,
+		required: true
+	}
 })
 
 const Article = mongoose.model("Article", articleSchema)
@@ -26,6 +32,27 @@ app.get("/articles", function (req, res) {
 			res.send("There are no articles presently ")
 		}
 	})
+})
+
+app.post("/articles", (req, res) => {
+	const title = req.body.title
+	const content = req.body.content
+
+	const newArticle = new Article({
+		title,
+		content
+	})
+
+	newArticle
+		.save()
+		.then(() => {
+			console.log("Successfully created an article!")
+			res.send("Successfully created an article!")
+		})
+		.catch((err) => {
+			// console.log(err)
+			res.send("Error 400, Bad Request")
+		})
 })
 
 app.listen(4000, function () {
